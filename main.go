aca03ec5b88433ca90656d43007b1ecbad393a99
pisteuralpin <roulier.mathurin@gmail.com>
2025-09-16 19:29:11 +0200

    🔄️ Move to Goroutines

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"ha-rpi-monitoring/v0.1/lib/env"
 	"time"
 )
 
 func main() {
 	fmt.Println("Starting Raspberry Pi Monitoring...")
 
+	env.LoadEnv()
+
+	initConfig()
+
+	fmt.Println("✅ Raspberry Pi Monitoring started.")
+
+	// CPU Temperature
+	if env.GetEnvAsBool("CPU_TEMPERATURE_ENABLED", false) {
+		go monitorCPUTemperature()
+	}
+
+	// Prevent the main function from exiting
+	select {}
+}
+
+func monitorCPUTemperature() {
 	for {
 		temp, err := readCPUTemperature()
 		if err != nil {
@@ -17,6 +34,6 @@ func main() {
 		fmt.Println("CPU Temperature:", fmt.Sprintf("%.2f", temp), "°C")
 
 		// wait for the configured interval before reading again
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Duration(env.GetEnvAsInt("CPU_TEMPERATURE_INTERVAL", 1000)) * time.Millisecond)
 	}
 }
